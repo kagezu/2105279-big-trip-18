3bf7bd67f1611fd901b4e39b01762d6cbe565568
hinto <hinto@rambler.ru>
2022-08-24 15:47:53 +0700

    Передаёт данные в предствление для редактирования

--- src/view/edit-event-view.js
+++ src/view/edit-event-view.js
@@ -1,71 +1,46 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import { transformFirstCharToUpperCase } from '../utils.js';
+import { OFFERS_TYPE } from '../const.js';
 
-const createTemplate = () => `
+const createTypeIcon = (type) => `
+  <label class="event__type  event__type-btn" for="event-type-toggle-1">
+    <span class="visually-hidden">Choose event type</span>
+    <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
+  </label>
+  <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">`;
+const createTypeItem = (type, isChecked) => `
+  <div class="event__type-item">
+    <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio"
+    name="event-type" value="${type}"
+    ${isChecked ? ' checked' : ''}>
+      <label class="event__type-label  event__type-label--${type}"
+      for="event-type-${type}-1">${transformFirstCharToUpperCase(type)}</label>
+  </div>`;
+const createTypeList = (type) => {
+  const checkboxList = OFFERS_TYPE
+    .map((currentType) => createTypeItem(currentType, currentType === type))
+    .join('');
+  return `
+  <div class="event__type-list">
+    <fieldset class="event__type-group">
+      <legend class="visually-hidden">Event type</legend>
+      ${checkboxList}
+    </fieldset>
+  </div>`;
+};
+
+const createTemplate = ({ basePrice, dateFrom, dateTo, isFavorite, offers, type }) => `
   <li class="trip-events__item">
     <form class="event event--edit" action="#" method="post">
       <header class="event__header">
         <div class="event__type-wrapper">
-          <label class="event__type  event__type-btn" for="event-type-toggle-1">
-            <span class="visually-hidden">Choose event type</span>
-            <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
-          </label>
-          <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
-
-            <div class="event__type-list">
-              <fieldset class="event__type-group">
-                <legend class="visually-hidden">Event type</legend>
-
-                <div class="event__type-item">
-                  <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
-                    <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-bus-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="bus">
-                    <label class="event__type-label  event__type-label--bus" for="event-type-bus-1">Bus</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-train-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="train">
-                    <label class="event__type-label  event__type-label--train" for="event-type-train-1">Train</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-ship-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="ship">
-                    <label class="event__type-label  event__type-label--ship" for="event-type-ship-1">Ship</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-drive-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="drive">
-                    <label class="event__type-label  event__type-label--drive" for="event-type-drive-1">Drive</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight" checked>
-                    <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-check-in-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="check-in">
-                    <label class="event__type-label  event__type-label--check-in" for="event-type-check-in-1">Check-in</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-sightseeing-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="sightseeing">
-                    <label class="event__type-label  event__type-label--sightseeing" for="event-type-sightseeing-1">Sightseeing</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-restaurant-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="restaurant">
-                    <label class="event__type-label  event__type-label--restaurant" for="event-type-restaurant-1">Restaurant</label>
-                </div>
-              </fieldset>
-            </div>
+        ${createTypeIcon(type)}
+        ${createTypeList(type)}
         </div>
 
         <div class="event__field-group  event__field-group--destination">
           <label class="event__label  event__type-output" for="event-destination-1">
-            Flight
+           ${transformFirstCharToUpperCase(type)}
           </label>
           <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
             <datalist id="destination-list-1">
@@ -158,8 +133,16 @@ const createTemplate = () => `
       </li>`;
 
 export default class EditEventView extends AbstractView {
+  #point;
+  #offerModel;
+
+  constructor(point, offerModel) {
+    super();
+    this.#point = point;
+    this.#offerModel = offerModel;
+  }
 
   get template() {
-    return createTemplate();
+    return createTemplate(this.#point);
   }
 }
