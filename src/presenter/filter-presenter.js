70355ed0a61f9b232349d8d950295269b78ba5fc
hinto <hinto@rambler.ru>
2022-08-25 14:04:29 +0700

    Рефакторинг

--- src/presenter/filter-presenter.js
+++ src/presenter/filter-presenter.js
@@ -2,9 +2,9 @@ import { render, RenderPosition } from '../framework/render.js';
 import FiltersView from '../view/filters-view.js';
 
 export default class FilterPresenter {
-
+  #container;
   init = (container) => {
-    this.container = container;
-    render(new FiltersView(), this.container, RenderPosition.AFTERBEGIN);
+    this.#container = container;
+    render(new FiltersView(), this.#container, RenderPosition.AFTERBEGIN);
   };
 }
--- src/presenter/info-presenter.js
+++ src/presenter/info-presenter.js
@@ -2,9 +2,9 @@ import { render, RenderPosition } from '../framework/render.js';
 import TripInfoView from '../view/trip-info-view.js';
 
 export default class InfoPresenter {
-
+  #container;
   init = (container) => {
     this.container = container;
-    render(new TripInfoView(), this.container, RenderPosition.AFTERBEGIN);
+    render(new TripInfoView(), this.#container, RenderPosition.AFTERBEGIN);
   };
 }
