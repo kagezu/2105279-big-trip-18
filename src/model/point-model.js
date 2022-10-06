cfb5bc9a2b1b5aebd1572086ecdf3eff59e5ec8e
hinto <hinto@rambler.ru>
2022-10-06 13:18:35 +0700

    Пункт 7.10 Модель 2

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -1,7 +1,49 @@
 import { POINTS } from '../mock/generation.js';
+import Observable from '../framework/observable.js';
 
-export default class PointModel {
+export default class PointModel extends Observable {
   #points = POINTS;
 
   get = () => this.#points;
+
+  updatePoint = (updateType, update) => {
+    const index = this.#points.findIndex((task) => task.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting points');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      update,
+      ...this.#points.slice(index + 1),
+    ];
+
+    this._notify(updateType, update);
+  };
+
+  addPoints = (updateType, update) => {
+    this.#points = [
+      update,
+      ...this.#points,
+    ];
+
+    this._notify(updateType, update);
+  };
+
+  deletePoints = (updateType, update) => {
+    const index = this.#points.findIndex((task) => task.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting points');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      ...this.#points.slice(index + 1),
+    ];
+
+    this._notify(updateType);
+  };
+
 }
